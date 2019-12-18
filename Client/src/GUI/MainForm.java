0569d9202fc047ddfc22e2e0ed9b43c85eef8460
km.Chrysalis <pulse66689@gmail.com>
2019-12-18 13:44:18 +0200

    Logo fixed Log out button placed down

--- Client/src/GUI/MainForm.java
+++ Client/src/GUI/MainForm.java
@@ -4,6 +4,6 @@ import WindowApp.IcmForm;
 import javafx.fxml.Initializable;
 import javafx.stage.Stage;
 
-public interface   MainForm    {
-	public abstract void start(Stage primaryStage) throws Exception ;
+public interface MainForm {
+	void start(Stage primaryStage) throws Exception ;
 }
---
+++ Client/src/GUI/MainMenuForm.java
@@ -0,0 +1,19 @@
+package GUI;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+
+public class MainMenuForm {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void initialize() {
+
+    }
+}
