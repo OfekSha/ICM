90c1f8d417c9dc1b85ec5184618f36e35fbf9b6e
JonnyGov <50523757+JonnyGov@users.noreply.github.com>
2019-12-27 16:14:09 +0200

    Permissions advancements (read description )
    -can know add/remove a single Permission for user
    -main menu know shows buttons according to users Permissions

--- Client/src/Entity/User.java
+++ Client/src/Entity/User.java
@@ -5,124 +5,158 @@ import java.util.ArrayList;
 import java.util.EnumSet;
 
 /**
- * @author Yonathan -TODO: test it 
+ * @author Yonathan 
+ *  entity containing all of the users details  
+ *  Also enforces some of the story's constraints 
  *
  */
 public class User implements Serializable {
-	
+
 	// enums*********************************************
-	public  static enum ICMPermissions{
-		informationTecnologiesDeparmentManger,
-		inspector,
-		estimator,
-		exeutionLeader,
-		examiner,
+	public static enum ICMPermissions {
+		informationTecnologiesDeparmentManger, inspector, estimator, exeutionLeader, examiner,
 		changeControlCommitteeChairman
 	}
-	
-	// wil be used to determan who can be allocted ICMPermissions
-	public enum Job{
-		student,
-		informationEngineer
-		
-	}
-	
-	
-	//Variables
-private String userName;
-private String password;
-private String firstName;
-private String lastName;
-private String email ;
-private EnumSet<ICMPermissions> Permissions =null;
-//private ICMPermissions[] Permissions;
-private Job job ;
-private boolean logedIn;
 
+	// wil be used to determan who can be allocted ICMPermissions
+	public enum Job {
+		student, informationEngineer
+
+	}
+
+	// Variables
+	private String userName;
+	private String password;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private EnumSet<ICMPermissions> Permissions = null;
+//private ICMPermissions[] Permissions;
+	private Job job;
+	private boolean logedIn;
 
 // Contractors
 
+	/**
+	 * creates user entity while enforcing constraints
+	 * 
+	 * @param userName
+	 * @param password
+	 * @param firstName
+	 * @param lastName
+	 * @param email
+	 * @param job         -(enum-Job)
+	 * @param Permissions - (enum-ICMPermissions)if the user is is a student send
+	 *                    null
+	 * @param logedIn     - represents if the user is logged in
+	 */
+	public User(String userName, String password, String firstName, String lastName, String email, Job job,
+			EnumSet<ICMPermissions> Permissions, boolean logedIn) {
 
-
-/** 
- * creates user entity while enforcing constraints
- * 
- * @param userName
- * @param password
- * @param firstName
- * @param lastName
- * @param email
- * @param job			-(enum-Job)
- * @param Permissions  -  (enum-ICMPermissions)if the user is is a student send null
- * @param logedIn		- represents if the user is logged in
- */
-public User(String userName ,String password,String firstName,String lastName ,String email,Job job,EnumSet<ICMPermissions> Permissions,boolean logedIn) {
-	
-	this.job=job;
-	try {
-	if (!(updatePremiisions(Permissions)))  throw new IllegalArgumentException("Students dont have Permissions in the system");
+		this.job = job;
+		try {
+			if (!(updatePremiisions(Permissions)))
+				throw new IllegalArgumentException("Students dont have Permissions in the system");
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		}
+		this.userName = userName;
+		this.password = password;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.logedIn = logedIn;
 	}
-	catch(IllegalArgumentException e) {
-		 e.printStackTrace();
-	}
-	this.userName =userName;
-	this.password =password;
-	this.firstName =firstName;
-	this.lastName =lastName;
-	this.email =email;
-	this.logedIn =logedIn;
-} 
-
 
 // input 
 
+	/**
+	 * updates all of the user entity Permissions while enforcing constraints
+	 * 
+	 * @param Permissions - EnumSet of the Permissions the user has
+	 * @return - returns true if the update was done
+	 */
+	public boolean updatePremiisions(EnumSet<ICMPermissions> Permissions) {
+		if (Permissions == null || job == Job.student)
+			return false;
+		this.Permissions = Permissions;
+		return true;
+	}
 
+	public void updateLoging(boolean logedIn) {
+		this.logedIn = logedIn;
+	}
 
-/**
- * updatedes the user entity Permissions while   enforcing constraints
- * @param Permissions - EnumSet of the Permissions the user has
- * @return				- returns  true if the update was done
- */
-public  boolean updatePremiisions(EnumSet<ICMPermissions> Permissions) {
-	if(Permissions==null || job==Job.student) return false;
-	 this.Permissions=Permissions; 
-	 return true;
-}
+	//TODO test this  method
+	/** adding a Permission to user entity 
+	 * @param Permission
+	 * @return  - returns true if permission is in the collection
+	 */
+	public boolean addPremmision(ICMPermissions Permission) {
 
-public void updateLoging(boolean logedIn) {
-	this.logedIn=logedIn;
-}
+		if (job == Job.student)
+			return false;
+		try {
+			if (Permission == null)
+				throw new IllegalArgumentException("null  is not a Permission");
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+			System.exit(0);				// must exit -EnumSet cannot receive null
+		}
 
+		if (this.Permissions == null) {
+			EnumSet<ICMPermissions> all = EnumSet.allOf(ICMPermissions.class);
+			this.Permissions = EnumSet.complementOf(all);
+			this.Permissions.add(Permission);
+			return true;
+		}
+		if (this.Permissions.contains(Permission))
+			return true;
+		this.Permissions.add(Permission);
+		return true;
+
+	}  // END of  addPremmision()
+	
+	
+	/** removes the Permission from the user
+	 * @param Permission
+	 */
+	public void removePermission(ICMPermissions Permission) {
+		if (Permission == null || this.Permissions == null) return ; 
+		if (this.Permissions.contains(Permission)) this.Permissions.remove(Permission); 
+	}// END of removePermission()
 
 //output
-public String getUserName() {
-	return userName;
-} 
-public String getPassword() {
-	return password;
-} 
-public String getFirstName() {
-	return firstName;
-} 
-public String getLastName() {
-	return lastName;
-} 
-public String getEmail() {
-	return email;
-} 
-
-public EnumSet<ICMPermissions> getICMPermissions() {
-	return Permissions;
-}
-
-public Job getJob() {
-	return job;
+	public String getUserName() {
+		return userName;
 	}
-public boolean getlogedIn() {
-	return logedIn;
-}
 
+	public String getPassword() {
+		return password;
+	}
 
+	public String getFirstName() {
+		return firstName;
+	}
 
+	public String getLastName() {
+		return lastName;
+	}
 
-}//End of User
+	public String getEmail() {
+		return email;
+	}
+
+	public EnumSet<ICMPermissions> getICMPermissions() {
+		return Permissions;
+	}
+
+	public Job getJob() {
+		return job;
+	}
+
+	public boolean getlogedIn() {
+		return logedIn;
+	}
+
+}// End of User
--- Client/src/GUI/MainMenuForm.java
+++ Client/src/GUI/MainMenuForm.java
@@ -1,9 +1,11 @@
 package GUI;
 
 import java.net.URL;
+import java.util.EnumSet;
 import java.util.ResourceBundle;
 
 import Entity.User;
+import Entity.User.ICMPermissions;
 import WindowApp.ClientLauncher;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -41,51 +43,80 @@ public class MainMenuForm extends UserForm {
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		ClientLauncher.client.setClientUI(this);
-		
-		/*  @yonathan -work in proggress 
-		//TODO: move to sec controller 
-		
-		
-		User.ICMPermissions[] perArr = user.getICMPermissions();
-		for( User.ICMPermissions p: perArr) {}
-		//
-		 */
-	}
-	
 
-	//TODO: Load suitable list for each new form
+		// access according to Permissions
+		btnInformationTechnologiesDepartmentManager.setDisable(true);
+		btnInspector.setDisable(true);
+		btnEstimator.setDisable(true);
+		btnExecutionLeader.setDisable(true);
+		btnExaminer.setDisable(true);
+		btnChangeControlCommitteeChairman.setDisable(true);
+
+		EnumSet<ICMPermissions> Permissions = user.getICMPermissions();
+		for (User.ICMPermissions p : Permissions) {
+			switch (p) {
+			case informationTecnologiesDeparmentManger:
+				btnInformationTechnologiesDepartmentManager.setDisable(false);
+				break;
+			case inspector:
+				btnInspector.setDisable(false);
+				break;
+			case estimator:
+				btnEstimator.setDisable(false);
+				break;
+			case exeutionLeader:
+				btnExecutionLeader.setDisable(false);
+				break;
+			case examiner:
+				btnExaminer.setDisable(false);
+				break;
+			case changeControlCommitteeChairman:
+				btnChangeControlCommitteeChairman.setDisable(false);
+				break;
+			}
+		}
+
+	} // END of initialize();
+
+	// TODO: Load suitable list for each new form
 	public void WatchRequest(ActionEvent event) throws Exception {
 		getRequests();
-		NextWindowLauncher(event, "/GUI/WatchRequest.fxml", this,  true);
+		NextWindowLauncher(event, "/GUI/WatchRequest.fxml", this, true);
 	}
 
 	public void MakeAChangeRequest(ActionEvent event) throws Exception {
-		//NextWindowLauncher(event, "/GUI/MainMenuForm.fxml", this, new WatchRequestForm(), true);
+		// NextWindowLauncher(event, "/GUI/MainMenuForm.fxml", this, new
+		// WatchRequestForm(), true);
 	}
 
 	public void InformationTechnologiesDepartmentManagerMenu(ActionEvent event) throws Exception {
-		//NextWindowLauncher(event, "/GUI/MainMenuForm.fxml", this, new WatchRequestForm(), true);
+		// NextWindowLauncher(event, "/GUI/MainMenuForm.fxml", this, new
+		// WatchRequestForm(), true);
 	}
 
 	public void InspectorMenu(ActionEvent event) throws Exception {
-		NextWindowLauncher(event, "/GUI/InspectorMain.fxml", this,  true);
+		NextWindowLauncher(event, "/GUI/InspectorMain.fxml", this, true);
 
 	}
 
 	public void EstimatorMenu(ActionEvent event) throws Exception {
-	//	NextWindowLauncher(event, "/GUI/MainMenuForm.fxml", this, new WatchRequestForm(), true);
+		// NextWindowLauncher(event, "/GUI/MainMenuForm.fxml", this, new
+		// WatchRequestForm(), true);
 	}
 
 	public void ExecutionLeaderMenu(ActionEvent event) throws Exception {
-		//NextWindowLauncher(event, "/GUI/MainMenuForm.fxml", this, new WatchRequestForm(), true);
+		// NextWindowLauncher(event, "/GUI/MainMenuForm.fxml", this, new
+		// WatchRequestForm(), true);
 	}
 
 	public void ExaminerMenu(ActionEvent event) throws Exception {
-		//NextWindowLauncher(event, "/GUI/MainMenuForm.fxml", this, new RequestForm(), true);
+		// NextWindowLauncher(event, "/GUI/MainMenuForm.fxml", this, new RequestForm(),
+		// true);
 	}
 
 	public void ChangeControlCommitteeChairmanMenu(ActionEvent event) throws Exception {
-		//NextWindowLauncher(event, "/GUI/MainMenuForm.fxml", this, new WatchRequestForm(), true);
+		// NextWindowLauncher(event, "/GUI/MainMenuForm.fxml", this, new
+		// WatchRequestForm(), true);
 	}
 
-}//END of MainMenuForm
+}// END of MainMenuForm
