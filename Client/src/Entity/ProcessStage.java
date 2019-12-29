83b2f344e8531dfa40060e3b80ec26a661e363cc
JonnyGov <50523757+JonnyGov@users.noreply.github.com>
2019-12-29 17:59:52 +0200

    added sub stages and time extention fallow up

--- Client/src/Entity/ProcessStage.java
+++ Client/src/Entity/ProcessStage.java
@@ -17,15 +17,29 @@ public class ProcessStage implements Serializable{
 		examination, // stage 4
 		closure // stage 5
 	}
+	 // not all stages have  all sub stages
+	public enum subStages{
+		supervisorAllocation,	 
+		// if inspector disapproved  due time you have to go back to this sub stage	 
+		DeterminingDueTime,		
+		supervisorAction
+	}
 
 // vars
 	private ChangeRequest Request;
 	private ChargeRequestStages currentStage = ChargeRequestStages.meaningEvaluation; // starting in the first stage
+	private subStages currentSubStage=subStages.supervisorAllocation;					// first stage subStage
 	private User StageSupervisor = null;
 	private EnumSet<ICMPermissions> Permissions = null;
 	private String EstimatorReport = "";
 	private String ExeminorFailReport = "";
 	private String inspectorDocumention = "";
+	
+	/**
+	 * an array whit what stages asked for extension
+	 * [stage number-1]
+	 */
+	private boolean[] WasThereAnExtentionRequest =new boolean[5]  ;
 
 	/**
 	 *  startEndArray is -an array with the start date and end date for each stage
@@ -41,18 +55,43 @@ public class ProcessStage implements Serializable{
 	
 	public ProcessStage(ChangeRequest Request) {
 		this.Request=Request;
+		for (int i =0 ;i<5;i++) {
+			WasThereAnExtentionRequest[i] =false;
+		}
 	}
 	// input methods
 	//  TODO: add constraints to date methods 
+
+	/** adding a start date to the current satge
+	 * @param start
+	 */
 	public void addStartDate (LocalDate start) {
 		startEndArray[currentStage.ordinal()][0] =start;
 		}
+	/**adding a due date to the current satge
+	 * @param due
+	 */
 	public void addDueDate (LocalDate due) {
 		startEndArray[currentStage.ordinal()][1] =due;
 		}
+	/**adding a end date to the current satge
+	 * @param end
+	 */
 	public void addEndDate (LocalDate end) {
 		startEndArray[currentStage.ordinal()][2] =end;
 		}
+	/** Extention Request Made was made at the current stage
+	 * 
+	 */
+	public void ExtentionRequestMade() {
+		WasThereAnExtentionRequest[currentStage.ordinal()]=true;
+	}
+	/** change the sub stage you are in
+	 * @param newSubStage
+	 */
+	public void changecurretSubStage(subStages newSubStage) {
+		  currentSubStage = newSubStage;
+	}
 	
 	
 	public void newStageSupervisor(User supervisor) {
@@ -158,5 +197,11 @@ public class ProcessStage implements Serializable{
 	public LocalDate[][] getDates() {
 		return startEndArray;
 	}
+	public boolean[] getWasThereAnExtentionRequest() {
+		return WasThereAnExtentionRequest;
+	}
+	public subStages getCurrentSubStage() {
+		 return currentSubStage;
+	} 
 
 }// END of Stage
