8a8fa44d669af616307d548a3b172d820ec6c1dc
ooffe <ooffe@LAPTOP-4ANHB4O7>
2019-12-30 18:44:08 +0200

    add inspector-- with errors.
    dont click inspector in gui

--- Client/src/Controllers/InspectorController.java
+++ Client/src/Controllers/InspectorController.java
@@ -2,10 +2,132 @@ package Controllers;
 
 import java.util.ArrayList;
 
+import javax.naming.directory.InvalidAttributesException;
+
+import java.time.LocalDate;
+
+import Entity.ChangeRequest;
+import Entity.ChangeRequest.ChangeRequestStatus;
+import Entity.ProcessStage;
+import Entity.ProcessStage.ChargeRequestStages;
+import Entity.clientRequestFromServer;
+import Entity.clientRequestFromServer.requestOptions;
+import GUI.InspectorForm;
+import WindowApp.ClientLauncher;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.scene.control.MenuItem;
+
 public class InspectorController {
+	public static class requirmentForTable {
+		private  SimpleStringProperty message; 
+		private  SimpleStringProperty  id;
+		private  SimpleObjectProperty<ChangeRequestStatus> status;
+		//private  SimpleStringProperty stage; // for test only
+		//private  SimpleStringProperty dueTime; // for test only
+		private SimpleObjectProperty<ProcessStage>stage;
+		private SimpleObjectProperty<LocalDate> dueTime;
+		
+		
+		public String getMessage() {
+			return message.get();
+		}
+		public String getId() {
+			return id.get();
+		}
+		public ChangeRequestStatus getStatus() {
+			return status.get();
+		}
+		public ProcessStage getStage() {
+			return stage.get();
+		}
+		public LocalDate getDueTime() {
+			return dueTime.get();
+		}
+		
+		
+		public requirmentForTable(ChangeRequest req) {
+			id=new SimpleStringProperty(req.getRequestID());
+			status = new SimpleObjectProperty<ChangeRequestStatus>(req.getStatus());
+			message= new SimpleStringProperty("test");
+			stage= new SimpleObjectProperty<ProcessStage>(req.stage);	
+		}
+		
+	}
+	public static ArrayList <requirmentForTable> requirmentForTableList(ArrayList<ChangeRequest> reqList) {
+		ArrayList <requirmentForTable> newList= new ArrayList<requirmentForTable>();
+		for (ChangeRequest req : reqList) newList.add(new requirmentForTable(req));
+		return newList;
+	}
+	// public ArrayList <lReport> getClosedRequests(){}
 	
-	//public ArrayList <lReport> getClosedRequests(){} jhgjgvvnmbmvjmvmnbvmnvcgm, ofek is the man
-	
-	
+	public static void freeze(requirmentForTable req) {
+		for (ChangeRequest request : InspectorForm.reqList)
+			if (request.getRequestID()==req.getId()) {
+				request.changeStatus(ChangeRequestStatus.suspended);
+			requestToServerProtocol(new clientRequestFromServer(requestOptions.updateStatus,request));
+			break;
+			}	
+	}
+	public static void unfreeze(requirmentForTable req) {
+		for (ChangeRequest request : InspectorForm.reqList)
+			if (request.getRequestID()==req.getId()) {
+				request.changeStatus(ChangeRequestStatus.ongoing);
+			requestToServerProtocol(new clientRequestFromServer(requestOptions.updateStatus,request));
+			break;
+			}	
+	}
+	enum reqFilter{
+		freeze,unfreeze,estimator,executionLeader,dueTime,close,extension
+	}
+	private static reqFilter filter;
+	public static void watchRequests(MenuItem item) {
+		switch (item.getText()) { // choose what string to send to server
+		case "Freeze Requests":
+			filter=reqFilter.freeze; // need set object
+			break;
+		case "Unfreeze Requests":
+			filter=reqFilter.unfreeze;
+			break;
+		case "Approve Estimator":
+			filter=reqFilter.estimator;
+			break;
+		case "Approve Execution Leader":
+			filter=reqFilter.executionLeader;
+			break;
+		case "Approve Due Time":
+			filter=reqFilter.dueTime;
+			break;
+		case "Waiting for close":
+			filter=reqFilter.close;
+			break;
+		case "Waiting for Extension":
+			filter=reqFilter.extension;
+			break;
+		}
+		clientRequestFromServer toServer = new clientRequestFromServer(requestOptions.getAll,filter); // create new
+		// request
+		// protocol to
+		// server
+		requestToServerProtocol(toServer);
+	}
+
+	private static void requestToServerProtocol(clientRequestFromServer req) { // send to server request protocol.
+		ClientLauncher.client.handleMessageFromClientUI((Object) req);
+	}
+
+	public static void messageFromServer(Object message) {
+		// @@ need to add testing for message
+		clientRequestFromServer respone = (clientRequestFromServer) message;
+		switch (respone.getRequest()) {
+		case getRequirement:
+			InspectorForm.reqList=respone.getObj();
+			break;
+		case getAll:
+			InspectorForm.reqList=respone.getObj();
+			break;
+		}
+	}
 
 }
--- Client/src/GUI/InspectorForm.java
+++ Client/src/GUI/InspectorForm.java
@@ -1,35 +1,178 @@
 package GUI;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
+import com.sun.corba.se.pept.transport.EventHandler;
+
+import Controllers.InspectorController;
+import Controllers.InspectorController.requirmentForTable;
+import Entity.ChangeRequest;
+import Entity.ChangeRequest.ChangeRequestStatus;
+import Entity.Requirement.statusOptions;
 import WindowApp.ClientLauncher;
 import WindowApp.IcmForm;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 public class InspectorForm extends UserForm implements IcmForm {
+	// fxml vars:
+	@FXML
+	public Button btnGetDetails;
+	@FXML
+	public Button btnFreezeUnfreeze;
+	@FXML
+	public Button btnRoleApprove;
+	@FXML
+	public Button btnDueTimeApprove;
+	@FXML
+	public Button btnExtensionApprove;
+	@FXML
+	public Button btnCloseRequest;
+
+	// menu button for watch types of requests
+	@FXML
+	public MenuButton menubtnWatch;
+
+	// menu items of menubtnWatch (the types of request):
+	@FXML
+	private MenuItem freeze;
+	@FXML
+	private MenuItem unfreeze;
+	@FXML
+	private MenuItem estimator;
+	@FXML
+	private MenuItem executionLeader;
+	@FXML
+	private MenuItem dueTime;
+	@FXML
+	private MenuItem close;
+	@FXML
+	private MenuItem extension;
+
+	@FXML
+	public TableView<requirmentForTable> tblviewRequests;
+	// table colums:
+	@FXML
+	public TableColumn<requirmentForTable, Integer> columnId;
+	@FXML
+	public TableColumn<requirmentForTable, Object> columnStatus;
+	@FXML
+	public TableColumn<requirmentForTable, Object> columnStage;
+	@FXML
+	public TableColumn<requirmentForTable, Object> columnDueTime;
+	
+	/*
+	//for test only:
+	@FXML
+	public TableColumn<requirmentForTable, String> columnStage;
+	@FXML
+	public TableColumn<requirmentForTable, String> columnDueTime;
+	*/
+	
+	
+	
+	@FXML
+	public TableColumn<requirmentForTable, String> columnMessage;
+
+	// not fxml vars:
+	public static ArrayList<ChangeRequest> reqList;
+	private ObservableList<requirmentForTable> tableData;
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		// TODO Auto-generated method stub
+
+		initializeTableView();
+
+	}
+	
+	private void initializeTableView() {
+		columnMessage.setCellValueFactory(new PropertyValueFactory<requirmentForTable, String>("message")); // set values for messages
+		columnId.setCellValueFactory(new PropertyValueFactory<requirmentForTable, Integer>("id")); // set values for id
+		columnStatus.setCellValueFactory(new PropertyValueFactory<requirmentForTable, Object>("status")); // set values for status
+		/*
+		//tests:
+		columnStage.setCellValueFactory(new PropertyValueFactory<requirmentForTable, String>("stage"));
+		columnDueTime.setCellValueFactory(new PropertyValueFactory<requirmentForTable, String>("dueTime"));
+		*/
+		
+		
+		columnStage.setCellValueFactory(new PropertyValueFactory<requirmentForTable,Object>("stage"));
+		columnDueTime.setCellValueFactory(new PropertyValueFactory<requirmentForTable, Object>("dueTime"));
+		//tblviewRequests.setItems(tableData);
+		
 	}
 
 	@Override
 	public void getFromServer(Object message) {
-		// TODO Auto-generated method stub
-		
+		InspectorController.messageFromServer(message);
+		tableData = FXCollections.observableArrayList(InspectorController.requirmentForTableList(reqList));
+		tblviewRequests.setItems(tableData);
+
 	}
 
-	//TODO: the following  methods are from the class diagram:  
+	// functions for gui:
+	public void watchRequest(ActionEvent event) throws Exception { // get event from the menuItem.
+		InspectorController.watchRequests(((MenuItem) event.getSource()));
+	}
+	public void freezeOrUnfreeze(ActionEvent event)throws Exception{
+		requirmentForTable selectedReq =tblviewRequests.getSelectionModel().getSelectedItem();
+		switch (selectedReq.getStatus()) {
+		// the requirement wasn't freeze.
+		case ongoing:
+			InspectorController.freeze(selectedReq);
+			btnFreezeUnfreeze.setText("Unfreeze");
+			break;
+		case suspended:
+			InspectorController.unfreeze(selectedReq);
+			btnFreezeUnfreeze.setText("freeze");
+			break;
+		default:throw new Exception("clicked freeze / unfreeze on request thats not ongoing or susspended status.");
+		}
+	}
 	
-	public void getUserId() {}
-	public void getRule() {}
-	public void getRequestId() {}
-	public void getDoneReport() {}
-	public void getTalkeApprove() {}
+	public void onRequirmentClicked(ActionEvent event)throws Exception{
+		requirmentForTable selectedReq =tblviewRequests.getSelectionModel().getSelectedItem();
+		switch (selectedReq.getStatus()) {
+		// the requirement wasn't freeze.
+		case ongoing:
+			btnFreezeUnfreeze.setDisable(false);
+			btnFreezeUnfreeze.setText("Unfreeze");
+			break;
+		case suspended:
+			btnFreezeUnfreeze.setDisable(false);
+			btnFreezeUnfreeze.setText("freeze");
+			break;
+		default:btnFreezeUnfreeze.setDisable(true);
+			break;
+		}
+	}
+	// TODO: the following methods are from the class diagram:
+
+	public void getUserId() {
+	}
+
+	public void getRule() {
+	}
+
+	public void getRequestId() {
+	}
+
+	public void getDoneReport() {
+	}
+
+	public void getTalkApprove() {
+	}
 
 }
