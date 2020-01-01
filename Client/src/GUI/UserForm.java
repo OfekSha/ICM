2d5bf01b19a9b0bdd25b57f6c4ebbc8800de9ee3
ooffe <ooffe@LAPTOP-4ANHB4O7>
2020-01-01 14:26:17 +0200

    deleted get request from userform

--- Client/src/GUI/UserForm.java
+++ Client/src/GUI/UserForm.java
@@ -115,7 +115,6 @@ public abstract class UserForm implements IcmForm {
 	public void NextWindowLauncher(ActionEvent event, String path,
 								   IcmForm launcherClass, boolean hide) throws Exception {
 		if (hide) {
-			getRequests();
 			((Node) event.getSource()).getScene().getWindow().hide(); // hiding primary window
 		}
 		Stage stage = new Stage();
