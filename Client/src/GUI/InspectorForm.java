dc9fdd71b877646a9f5057d2f4529f9195125d49
Ofek Shachar <48984874+OfekSha@users.noreply.github.com>
2020-01-11 18:39:34 +0200

    update inspector form and main

--- Client/src/GUI/InspectorForm.java
+++ Client/src/GUI/InspectorForm.java
@@ -2,6 +2,7 @@ package GUI;
 
 import Controllers.InspectorController;
 import Entity.ChangeRequest;
+import Entity.EstimatorReport;
 import Entity.ProcessStage.ChargeRequestStages;
 import Entity.RequestTableView;
 import Entity.RequestTableView.requirementForTable;
@@ -20,6 +21,7 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
 
@@ -37,7 +39,108 @@ public class InspectorForm extends UserForm {
 	public Button btnExtensionApprove;
 	@FXML
 	public Button btnCloseRequest;
+	//fxml for tab pan:
+	@FXML
+    private TextArea requestDetails;
 
+    @FXML
+    private TextArea requestReason;
+
+    @FXML
+    private TextArea comment;
+
+    @FXML
+    private TextField informationSystem;
+
+    @FXML
+    private TableView<?> tblViewDocuments;
+
+    @FXML
+    private TableColumn<?, ?> columnFileName;
+
+    @FXML
+    private TableColumn<?, ?> columnFileSize;
+
+    @FXML
+    private Button donwloadBtn;
+
+    @FXML
+    private TextArea location;
+
+    @FXML
+    private TextArea changeDescription;
+
+    @FXML
+    private TextArea desiredResult;
+
+    @FXML
+    private TextArea constraints;
+
+    @FXML
+    private TextArea risks;
+
+    @FXML
+    private TextField dueTimeEstimate;
+
+    @FXML
+    private TextField start1;
+
+    @FXML
+    private TextField end1;
+
+    @FXML
+    private CheckBox extension1;
+
+    @FXML
+    private TextArea explain1;
+
+    @FXML
+    private TextField start2;
+
+    @FXML
+    private TextField end2;
+
+    @FXML
+    private CheckBox extension2;
+
+    @FXML
+    private TextArea explain2;
+
+    @FXML
+    private TextField start3;
+
+    @FXML
+    private TextField end3;
+
+    @FXML
+    private CheckBox extension3;
+
+    @FXML
+    private TextArea explain3;
+
+    @FXML
+    private TextField start4;
+
+    @FXML
+    private TextField end4;
+
+    @FXML
+    private CheckBox extension4;
+
+    @FXML
+    private TextArea explain4;
+
+    @FXML
+    private TextField start5;
+
+    @FXML
+    private TextField end5;
+
+    @FXML
+    private CheckBox extension5;
+
+    @FXML
+    private TextArea explain5;
 	// menu button for watch types of requests
 	@FXML
 	public MenuButton menubtnWatch;
@@ -175,6 +278,73 @@ public class InspectorForm extends UserForm {
 	public void onRequirementClicked(MouseEvent event) {
 		 selectedReq = table.onRequirementClicked(event);
 		 InspectorController.selectedRequest=selectedReq.getOriginalRequest();
+		 //set details into tab pane:
+		// requestDetails.setText(InspectorController.selectedRequest.);
+		 requestReason.setText(InspectorController.selectedRequest.getChangeReason());
+		 comment.setText(InspectorController.selectedRequest.getComment());
+		 informationSystem.setText(InspectorController.selectedRequest.getSystem());
+		 if (InspectorController.selectedRequest.getProcessStage().getEstimatorReport()!=null) {
+			 EstimatorReport estimatorReport = InspectorController.selectedRequest.getProcessStage().getEstimatorReport();
+			 location.setText(estimatorReport.getlocation());
+			 changeDescription.setText(estimatorReport.getChangeDescription());
+			 desiredResult.setText(estimatorReport.getResultingResult());
+			 constraints.setText(estimatorReport.getConstraints());
+			 risks.setText(estimatorReport.getRisks());
+			 dueTimeEstimate.setText(estimatorReport.getTimeEstimate().toString());
+		 }
+		 String[] explanations = selectedReq.getStage().getAllExtensionExplanation();
+		LocalDate[][] allDates = selectedReq.getStage().getDates();
+		 start1.setText("");
+		 end1.setText("");
+		 explain1.setText("");
+		 extension1.setSelected(false);
+		 start2.setText("");
+		 end2.setText("");
+		 explain2.setText("");
+		 extension2.setSelected(false);
+		 start3.setText("");
+		 end3.setText("");
+		 explain3.setText("");
+		 extension3.setSelected(false);
+		 start4.setText("");
+		 end4.setText("");
+		 explain4.setText("");
+		 extension4.setSelected(false);
+		 start5.setText("");
+		 end5.setText("");
+		 explain5.setText("");
+		 extension5.setSelected(false);
+		if (selectedReq.getStage().getWasThereAnExtensionRequest()[0]==2) {
+		 start1.setText(allDates[0][0].toString());
+		 end1.setText(allDates[0][2].toString());
+		 explain1.setText(explanations[0]);
+		 extension1.setSelected(true);
+		}
+		if (selectedReq.getStage().getWasThereAnExtensionRequest()[1]==2) {
+		 start2.setText(allDates[1][0].toString());
+		 end2.setText(allDates[1][2].toString());
+		 explain2.setText(explanations[1]);
+		 extension2.setSelected(true);
+		}
+		if (selectedReq.getStage().getWasThereAnExtensionRequest()[2]==2) {
+		 start3.setText(allDates[2][0].toString());
+		 end3.setText(allDates[2][2].toString());
+		 explain3.setText(explanations[2]);
+		 extension3.setSelected(true);
+		}
+		if (selectedReq.getStage().getWasThereAnExtensionRequest()[3]==2) {
+		 start4.setText(allDates[3][0].toString());
+		 end4.setText(allDates[3][2].toString());
+		 explain4.setText(explanations[3]);
+		 extension4.setSelected(true);
+		}
+		if (selectedReq.getStage().getWasThereAnExtensionRequest()[4]==2) {
+		 start5.setText(allDates[4][0].toString());
+		 end5.setText(allDates[4][2].toString());
+		 explain5.setText(explanations[4]);
+		 extension5.setSelected(true);
+		}
+		 
 		if (selectedReq == null)
 			return;
 		btnGetDetails.setDisable(false);
