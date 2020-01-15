8d93906f100c42ef1874558be589cdc94afc486c
JonnyGov <50523757+JonnyGov@users.noreply.github.com>
2020-01-15 13:52:03 +0200

    Revert "other refactoring"
    This reverts commit 53102ab63979ec0afaa3028a07a8046a1d994a85.

--- Client/src/GUI/DocmentTableForDownloadsForm.java
+++ Client/src/GUI/DocmentTableForDownloadsForm.java
@@ -1,6 +1,10 @@
 package GUI;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import Controllers.DocmentTableForDownloadsController;
+import Controllers.InspectorController;
 import Entity.ChangeRequest;
 import Entity.DocumentForTable;
 import javafx.collections.FXCollections;
@@ -11,7 +15,7 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 
-public class DocumentTableForDownloadsForm {
+public class DocmentTableForDownloadsForm   {
 
 	@FXML
 	public TableView<DocumentForTable> tblViewDocuments;
@@ -24,17 +28,18 @@ public class DocumentTableForDownloadsForm {
 
 	@FXML
 	public Button btnDownload;
-
-	public DocmentTableForDownloadsController controller = new DocmentTableForDownloadsController();
+	
+	public DocmentTableForDownloadsController controller =new DocmentTableForDownloadsController();
 
 	public void onRequirementTableClick(ChangeRequest selectedRequest) {
 		ObservableList<DocumentForTable> documentTableData = FXCollections
 				.observableArrayList(controller.DocumentForTableList(selectedRequest));
 		tblViewDocuments.setItems(documentTableData);
 	}// END of onRequirementTableClick()
-
+	
 	/**
 	 * setting up the document table columns
+	 * 
 	 */
 	public void initializeDocumentTableView() {
 
@@ -42,12 +47,13 @@ public class DocumentTableForDownloadsForm {
 		columnFileSize.setCellValueFactory(new PropertyValueFactory<>("size")); // set values
 
 	} //END of initializeDocumentTableView()
-
-	public void pressedDownload() {
+	
+	public void pressedDownload() {	
 		DocumentForTable selectedDoc = tblViewDocuments.getSelectionModel().getSelectedItem();
 		if (selectedDoc != null) {
 			controller.askForDownload(selectedDoc.gettheDoc());
 		}
 
-	} //END of pressedDownload()
+	} //END of pressedDownlode()
+
 }
--- Client/src/GUI/EstimatorMainForm.java
+++ Client/src/GUI/EstimatorMainForm.java
@@ -1,11 +1,15 @@
 package GUI;
 
 import Controllers.EstimatorController;
+import Controllers.InspectorController;
+import Entity.Document;
 import Entity.RequestTableView;
 import Entity.RequestTableView.requirementForTable;
 import GUI.PopUpWindows.DueTimeController;
 import GUI.PopUpWindows.GetExtensionController;
 import WindowApp.ClientLauncher;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -18,157 +22,147 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class EstimatorMainForm extends UserForm {
 
+	
+	@FXML
+	public DocmentTableForDownloadsForm  DocmentTableController; // the document table with download capability 
+		@FXML
+		public Button btnGetDetails;
+		@FXML
+		public Button btnSetDueTime;
+		@FXML
+		public Button btnWriteReport;
+		@FXML
+		public Button btnAskForTimeExtension;
+		
+		// menu button for watch types of requests
+		@FXML
+		public MenuButton menubtnWatch;
 
-	@FXML
-	public DocumentTableForDownloadsForm DocumentTableController; // the document table with download capability
-	@FXML
-	public Button btnGetDetails;
-	@FXML
-	public Button btnSetDueTime;
-	@FXML
-	public Button btnWriteReport;
-	@FXML
-	public Button btnAskForTimeExtension;
+		// menu items of menubtnWatch (the types of request):
+		@FXML
+		private MenuItem DueTimeFilter;
+		@FXML
+		private MenuItem ReportFilter;
+		//initiator details:
+		@FXML
+	    private TextArea RequestDetails;
 
-	// menu button for watch types of requests
-	@FXML
-	public MenuButton menubtnWatch;
+	    @FXML
+	    private TextArea RequestReason;
 
-	// menu items of menubtnWatch (the types of request):
-	@FXML
-	private MenuItem DueTimeFilter;
-	@FXML
-	private MenuItem ReportFilter;
-	//initiator details:
-	@FXML
-	private TextArea RequestDetails;
+	    @FXML
+	    private TextArea Comment;
 
-	@FXML
-	private TextArea RequestReason;
+	    @FXML
+	    private TextField system;
 
-	@FXML
-	private TextArea Comment;
+	    @FXML
+	    private TextField createdDate;
 
-	@FXML
-	private TextField system;
-
-	@FXML
-	private TextField createdDate;
-
-	@FXML
-	private TextField createdBy;
+	    @FXML
+	    private TextField createdBy;
 
 
-	//end initiator details.
-	private RequestTableView table;
-
-	@FXML
-	private TableView<requirementForTable> tblView;
-	@FXML
-	private TableColumn<requirementForTable, String> idColumn;
-	@FXML
-	private TableColumn<requirementForTable, Object> statusColumn;
-	@FXML
-	private TableColumn<requirementForTable, Object> dueTimeColumn;
-	@FXML
-	private TableColumn<requirementForTable, String> messageColumn;
-
-	public static Stage popupWindow;
-
-	@FXML
-	public void setDueTimeClicked(ActionEvent event) {
-		try {
-			// not work because the fxml work only with execution leader.
-			DueTimeController.processStage = EstimatorController.selectedRequest.getProcessStage();
-			popupWindow("/GUI/PopUpWindows/DeterminingDueTime.fxml", event);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			//TODO Warning:(91, 23) Throwable argument 'e.getCause()' to 'System.out.println()' call
-			System.out.println(e.getCause());
+	    //end initiator details.
+		private RequestTableView table;
+		
+		@FXML
+		private TableView tblView;
+		@FXML
+		private TableColumn idColumn;
+		@FXML
+		private TableColumn statusColumn;
+		@FXML
+		private TableColumn dueTimeColumn;
+		@FXML
+		private TableColumn messageColumn;
+		
+		public static Stage popupWindow;
+		@FXML
+		public void setDueTimeClicked(ActionEvent event) {
+			try {
+				// not work because the fxml work only with execution leader.
+				DueTimeController.processStage=EstimatorController.selectedRequest.getProcessStage();
+				popupWindow("/GUI/PopUpWindows/DeterminingDueTime.fxml",event);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				System.out.println(e.getCause());
+			}
 		}
-	}
-
-	@FXML
-	public void askExtensionClicked(ActionEvent event) {
-		try {
-			// not work because the fxml work only with execution leader.
-			GetExtensionController.processStage = EstimatorController.selectedRequest.getProcessStage();
-			popupWindow("/GUI/PopUpWindows/GetExtension.fxml", event);
-		} catch (IOException e) {
-			//TODO Warning:(103, 23) Throwable argument 'e.getCause()' to 'System.out.println()' call
-			System.out.println(e.getCause());
+		@FXML
+		public void askExtensionClicked(ActionEvent event) {
+			try {
+				// not work because the fxml work only with execution leader.
+				GetExtensionController.processStage=EstimatorController.selectedRequest.getProcessStage();
+				popupWindow("/GUI/PopUpWindows/GetExtension.fxml",event);
+			} catch (IOException e) {
+				System.out.println(e.getCause());
+			}
 		}
-	}
-
-	@FXML
-	private void writeReportClicked(ActionEvent event) {
-		try {
-			popupWindow("/GUI/PopUpWindows/EstimateReport.fxml", event);
-		} catch (IOException e) {
-			//TODO Warning:(113, 23) Throwable argument 'e.getCause()' to 'System.out.println()' call
-			System.out.println(e.getCause());
+		@FXML
+		private void writeReportClicked(ActionEvent event) {
+			try {
+				popupWindow("/GUI/PopUpWindows/EstimateReport.fxml",event);
+			} catch (IOException e) {
+				System.out.println(e.getCause());
+			}
 		}
-	}
+		private void popupWindow(String target, ActionEvent event) throws IOException {
+			popupWindow = new Stage();
+			Parent root = FXMLLoader.load(this.getClass().getResource(target));
+			Scene scene = new Scene(root);
+			popupWindow.setScene(scene);
+			popupWindow.initModality(Modality.APPLICATION_MODAL);
+			popupWindow.show();
+			EstimatorMainForm icmForm = this;
 
-	private void popupWindow(String target, ActionEvent event) throws IOException {
-		popupWindow = new Stage();
-		Parent root = FXMLLoader.load(this.getClass().getResource(target));
-		Scene scene = new Scene(root);
-		popupWindow.setScene(scene);
-		popupWindow.initModality(Modality.APPLICATION_MODAL);
-		popupWindow.show();
-		EstimatorMainForm icmForm = this;
+			// what happened when close window from out or from stage.close / stage.hide
+			// method
+			popupWindow.setOnCloseRequest(windowEvent ->  // close from out (alt +f4)
+						ClientLauncher.client.setClientUI(icmForm));
+			// stage.close / stage.hide method
+			popupWindow.setOnHidden(we -> ClientLauncher.client.setClientUI(icmForm));
+		}
+		@Override
+		public void initialize(URL arg0, ResourceBundle arg1) {
+			ClientLauncher.client.setClientUI(this);
+			table=new RequestTableView(tblView,idColumn,statusColumn,null,dueTimeColumn,messageColumn);
+			DocmentTableController.initializeDocumentTableView();
+			
+		}
+		@Override
+		public void getFromServer(Object message) {
+			EstimatorController.messageFromServer(message);
+			table.setData(EstimatorController.requests);
+			
+		}
+		public void filterRequests(ActionEvent event) { // get event from the menuItem.
+			EstimatorController.filterRequests(((MenuItem) event.getSource()));
+		}
+		public void onRequestClicked(MouseEvent event) {
 
-		// what happened when close window from out or from stage.close / stage.hide
-		// method
-		popupWindow.setOnCloseRequest(windowEvent ->  // close from out (alt +f4)
-				ClientLauncher.client.setClientUI(icmForm));
-		// stage.close / stage.hide method
-		popupWindow.setOnHidden(we -> ClientLauncher.client.setClientUI(icmForm));
-	}
-
-	@Override
-	public void initialize(URL arg0, ResourceBundle arg1) {
-		ClientLauncher.client.setClientUI(this);
-		table = new RequestTableView(tblView, idColumn, statusColumn, null, dueTimeColumn, messageColumn);
-		DocumentTableController.initializeDocumentTableView();
-
-	}
-
-	@Override
-	public void getFromServer(Object message) {
-		EstimatorController.messageFromServer(message);
-		table.setData(EstimatorController.requests);
-
-	}
-
-	public void filterRequests(ActionEvent event) { // get event from the menuItem.
-		EstimatorController.filterRequests(((MenuItem) event.getSource()));
-	}
-
-	public void onRequestClicked(MouseEvent event) {
-
-		requirementForTable selectedReq = table.onRequirementClicked(event);
-		if (selectedReq == null)
-			return;
-		EstimatorController.setSelectedRequest(selectedReq);
-
-		//set details for initiator:
-		RequestDetails.setText(EstimatorController.selectedRequest.getBaseforChange());
-		RequestReason.setText(EstimatorController.selectedRequest.getChangeReason());
-		Comment.setText(EstimatorController.selectedRequest.getComment());
-		system.setText(EstimatorController.selectedRequest.getSystem());
-		createdDate.setText(EstimatorController.selectedRequest.getStartDate().toString());
-		createdBy.setText(EstimatorController.selectedRequest.getInitiator().getTheInitiator().getUserName());
-		//end initiator details.
-		btnGetDetails.setDisable(false);
-		btnAskForTimeExtension.setDisable(false);
-		switch (selectedReq.getStage().getCurrentSubStage()) {
-			case determiningDueTime:
+			requirementForTable selectedReq = table.onRequirementClicked(event);
+			if (selectedReq == null)
+				return;
+			EstimatorController.setSelectedRequest(selectedReq);
+			
+			//set details for initiator:
+			RequestDetails.setText(EstimatorController.selectedRequest.getBaseforChange());
+			RequestReason.setText(EstimatorController.selectedRequest.getChangeReason());
+			Comment.setText(EstimatorController.selectedRequest.getComment());
+			system.setText(EstimatorController.selectedRequest.getSystem());
+			createdDate.setText(EstimatorController.selectedRequest.getStartDate().toString());
+			createdBy.setText(EstimatorController.selectedRequest.getInitiator().getTheInitiator().getUserName());
+			//end initiator details.
+			btnGetDetails.setDisable(false);
+			btnAskForTimeExtension.setDisable(false);
+			switch (selectedReq.getStage().getCurrentSubStage()) {
+			case determiningDueTime: 
 				btnSetDueTime.setDisable(false);
 				btnWriteReport.setDisable(true);
 				break;
@@ -181,7 +175,8 @@ public class EstimatorMainForm extends UserForm {
 				btnWriteReport.setDisable(true);
 				btnSetDueTime.setDisable(true);
 				break;
+			}
+			DocmentTableController.onRequirementTableClick(EstimatorController.selectedRequest);
 		}
-		DocumentTableController.onRequirementTableClick(EstimatorController.selectedRequest);
-	}
+		
 }
