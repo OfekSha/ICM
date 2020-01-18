48669694cb3cbdc755ba64afd1ce3921b9b524ad
JonnyGov <50523757+JonnyGov@users.noreply.github.com>
2020-01-19 00:49:50 +0200

    can know wach requests

--- Client/src/GUI/WatchRequestForm.java
+++ Client/src/GUI/WatchRequestForm.java
@@ -1,43 +1,91 @@
 package GUI;
 
 import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
-public class WatchRequestForm extends UserForm {
-	@FXML
-	public TextField tfRequestStatus;
-	public TextArea taRequestDetails;
-	public TextArea taRequestReason;
-	public TextArea taComment;
+import Controllers.InspectorController;
+import Controllers.StageSupervisorController;
+import Controllers.WatchRequestController;
+import Entity.ChangeRequest;
+import Entity.RequestTableView;
+import Entity.clientRequestFromServer;
+import Entity.clientRequestFromServer.requestOptions;
+import Entity.RequestTableView.requirementForTable;
+import WindowApp.ClientLauncher;
+import WindowApp.IcmForm;
+
+public class WatchRequestForm  extends StageSupervisorForm {
+	// fxml vars:
+
+	private ArrayList<ChangeRequest>  requests =new ArrayList<ChangeRequest>();
 
 
+		@FXML
+		private TableColumn<requirementForTable, Object> stageColumn;
+		private WatchRequestController controller = new WatchRequestController();
+		
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		getRequests();
-		Platform.runLater(this::setRequestsComboBox);
+		ClientLauncher.client.setClientUI(this);
+		table = new RequestTableView(tblView, idColumn, statusColumn, stageColumn, dueTimeColumn, messageColumn);
+		clientRequestFromServer toServer = new clientRequestFromServer(requestOptions.getAll, null);
+		ClientLauncher.client.handleMessageFromClientUI(toServer);
 	}
 
-	/*protected void setRequestsComboBox() {
-		ArrayList<String> al = new ArrayList<>();
-		changeRequests.forEach(cR -> al.add(cR.getRequestID()));
-		cmbRequests.setItems(FXCollections.observableArrayList(al));
-	}*/
-
-	public void RequestsComboBoxUsed() {
-		//int s = Integer.parseInt(cmbRequests.getSelectionModel().getSelectedItem());
-		String selected = cmbRequests.getSelectionModel().getSelectedItem();
-		changeRequests.forEach(cR -> {
-			if (selected.equals(cR.getRequestID())) {
-				this.tfRequestStatus.setText(cR.getStatus().name());
-				this.taRequestDetails.setText(cR.getProblemDescription());
-				this.taRequestReason.setText(cR.getChangeReason());
-				this.taComment.setText(cR.getComment());
-			}
-		});
+	@Override
+	public StageSupervisorController getController() {
+		// TODO Auto-generated method stub
+		return controller;
 	}
+
+	@Override
+	public IcmForm getIcmForm() {
+	return this;
+	}
+
+	@Override
+	public void filterRequests(ActionEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void onRequestClicked(MouseEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void getFromServer(Object message) {
+		clientRequestFromServer toServer =  (clientRequestFromServer) message;
+		requests = (ArrayList<ChangeRequest>) toServer.getObject();
+		ArrayList<ChangeRequest> toRemove =  new ArrayList<ChangeRequest>();
+		for (ChangeRequest e : requests)
+			if(!user.equals(e.getInitiator().getTheInitiator()))
+				toRemove.add(e);
+		for (ChangeRequest e : toRemove)
+			requests.remove(e);
+
+		table.setData(requests);
+		
+	}
+
+	@Override
+	public ChangeRequest getSelectedReq() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
 }
