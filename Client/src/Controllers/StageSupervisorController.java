7532a0ce5dfa1bdd14be79e046ae7d50e0098091
Tal <kotlertal22@gmail.com>
2020-01-18 03:19:01 +0200

    added approve disapprove and more details

--- Client/src/Controllers/StageSupervisorController.java
+++ Client/src/Controllers/StageSupervisorController.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 
 import Entity.ChangeRequest;
 import Entity.clientRequestFromServer;
+import Entity.ProcessStage.ChargeRequestStages;
 import Entity.ProcessStage.subStages;
 import Entity.RequestTableView.requirementForTable;
 import Entity.clientRequestFromServer.requestOptions;
@@ -51,4 +52,5 @@ public abstract class StageSupervisorController {
 	protected static void messageToServer(clientRequestFromServer req) { // send to server request protocol.
 		ClientLauncher.client.handleMessageFromClientUI((Object)req);
 	}
+
 }
