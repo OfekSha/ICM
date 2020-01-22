366134117be50b9964d979269b83d448aa36626d
Ofek Shachar <48984874+OfekSha@users.noreply.github.com>
2020-01-22 21:28:05 +0200

    Update ActivityReportSqlTest.java

--- Server/src/testSuit/ActivityReportSqlTest.java
+++ Server/src/testSuit/ActivityReportSqlTest.java
@@ -8,10 +8,13 @@ import org.junit.BeforeClass;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import com.sun.corba.se.impl.orb.ParserTable.TestContactInfoListFactory;
+
 import Entity.ActivitiesReport;
 import queryHandler.QueryHandler;
 import reporting.ReportController;
 import reporting.ReportController.reportScope;
+import testSuit.Status.statusEnum;
 import theServer.mysqlConnection;
 
 class ActivityReportSqlTest {
@@ -30,10 +33,27 @@ class ActivityReportSqlTest {
 	// TODO: add test to client send to server reports in 
 	@Test
 	 void insertReportToDBTest() {
+		int id;
+		String reportScope;
+		LocalDate start,end,created;
+		Status ongoingResults=new Status(statusEnum.ongoing);
 		ActivitiesReport report =reportController.creatActivitiesReport(LocalDate.now(), LocalDate.now().plusMonths(5), reportScope.dayOfweek);
 		qryHndlr.getActivitiesReportQuerys().InsertActivitiesReport(report);
 		fail();
 		
 	}
 
+}
+class Status{
+	enum statusEnum{
+		ongoing,suspended,close,rejected,treatment
+	}
+	statusEnum status;
+	float meidan,deviation,frequency;
+	int requests;
+	Status(statusEnum status){
+		this.status=status;
+		meidan=deviation=frequency=0;
+		requests=0;
+	}
 }
