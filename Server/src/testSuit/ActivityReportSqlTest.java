fef01f442e0bced3fd3a4a77fae3bb6bb7822170
Ofek Shachar <48984874+OfekSha@users.noreply.github.com>
2020-01-22 16:40:27 +0200

    Create ActivityReportSqlTest.java

---
+++ Server/src/testSuit/ActivityReportSqlTest.java
@@ -0,0 +1,14 @@
+package testSuit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ActivityReportSqlTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
